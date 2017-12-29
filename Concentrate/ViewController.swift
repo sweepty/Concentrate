08a557567263379117b73b3004fc48bf43958bd2
sweepty <adie0423@gmail.com>
2017-12-30 05:10:39 +0900

    Initial Commit

---
+++ Concentrate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentrate
+//
+//  Created by Seungyeon Lee on 2017. 12. 30..
+//  Copyright © 2017년 Seungyeon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
